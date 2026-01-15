c5338e86369debfb4aaf3939d1b852d0d4e38eba
mhenriksson <Mikkoh04@gmail.com>
2026-01-15 12:38:23 +0200

    2.4 tehty

--- tehtävä 2.4.py
+++ tehtävä 2.4.py
@@ -1,2 +1,11 @@
-numero = (input("kerro kolme kokonaislukua: ")
+eka = float(input("Anna ensimmäinen kokonaisluku: "))
+toka = float(input("Anna toinen kokonaisluku: "))
+kolmas =  float(input("Anna kolmas kokonaisluku: "))
 
+Summa = (eka + toka + kolmas)
+Tulo = (eka * toka * kolmas)
+Keskiarvo = ((eka + toka + kolmas) / 3)
+
+print(f"Kokonaislukujen summa on {Summa}")
+print(f"Kokonaislukujen tulo on {Tulo}")
+print(f"kokonaislukujen keskiarvo on {Keskiarvo}")
